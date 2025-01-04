a483212d9d3eb7d57b89ccbf3077d56cbef897c9
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2025-01-04 13:09:19 +0200

    coursedb update

--- business/domain/coursebus/stores/coursedb/coursedb.go
+++ business/domain/coursebus/stores/coursedb/coursedb.go
@@ -27,16 +27,76 @@ func NewStore(log *logger.Logger, db *sqlx.DB) *Store {
 	}
 }
 
+// NewWithTx constructs a new Store value replacing the sqlx DB
+// value with a sqlx DB value that is currently inside a transaction.
+func (s *Store) NewWithTx(tx sqldb.CommitRollbacker) (coursebus.Storer, error) {
+	ec, err := sqldb.GetExtContext(tx)
+	if err != nil {
+		return nil, err
+	}
+
+	store := Store{
+		log: s.log,
+		db:  ec,
+	}
+
+	return &store, nil
+}
+
 // Create inserts a new user into the database.
 func (s *Store) Create(ctx context.Context, cor coursebus.Course) error {
-	const q = `
-	INSERT INTO products
-		(product_id, user_id, name, cost, quantity, date_created, date_updated)
-	VALUES
-		(:product_id, :user_id, :name, :cost, :quantity, :date_created, :date_updated)`
 
-	if err := sqldb.NamedExecContext(ctx, s.log, s.db, q, toDBCourse(cor)); err != nil {
-		return fmt.Errorf("namedexeccontext: %w", err)
+	//insert course
+	query := `
+	INSERT INTO courses (
+		ID, instructorId, instructorName, date, title, category, level, primaryLanguage,
+		subtitle, description, image, welcomeMessage, pricing, objectives, isPublished
+	) VALUES (:ID, :instructorId, :instructorName, :date, :title, :category, :level, :primaryLanguage,
+		:subtitle, :description, :image, :welcomeMessage, :pricing, :objectives, :isPublished)`
+
+	if err := sqldb.NamedExecContext(ctx, s.log, s.db, query, toDBCourse(cor)); err != nil {
+		return fmt.Errorf("named exec context: %w", err)
+	}
+
+	//insert the students
+	if len(cor.Students) > 0 {
+		for _, student := range cor.Students {
+			query = `
+	           INSERT INTO Students (courseId, studentId, studentName, studentEmail, paidAmount)
+	           VALUES (:courseId, :studentId, :studentName, :studentEmail, :paidAmount)`
+
+			data := toDBStudent(student)
+
+			if err := sqldb.NamedExecContext(ctx, s.log, s.db, query, data); err != nil {
+				return fmt.Errorf("named exec context: %w", err)
+			}
+		}
+	}
+
+	//insert Lecture and link Lecture To Course
+	if len(cor.Curriculum) > 0 {
+		for _, lecture := range cor.Curriculum {
+			query = `
+			INSERT INTO lectures (lecture_id, title, videoUrl, public_id, freePreview)
+			VALUES (:lecture_id, :title, :videoUrl, :public_id, :freePreview)`
+
+			data := toDBLecture(lecture)
+
+			if err := sqldb.NamedExecContext(ctx, s.log, s.db, query, data); err != nil {
+				return fmt.Errorf("named exec context: %w", err)
+			}
+
+			//link Lecture To Course
+			query = `
+			INSERT INTO curriculum (course_id, lecture_id)
+			VALUES (:course_id, :lecture_id)`
+
+			curriculumData := toDBCurriculum(lecture.ID, cor.ID)
+
+			if err := sqldb.NamedExecContext(ctx, s.log, s.db, query, curriculumData); err != nil {
+				return fmt.Errorf("namedexeccontext: %w", err)
+			}
+		}
 	}
 
 	return nil
@@ -67,31 +127,68 @@ func (s *Store) Update(ctx context.Context, cor coursebus.Course) error {
 		return fmt.Errorf("namedexeccontext: %w", err)
 	}
 
+	//========================================================================================
+	// Delete existing lectures and add updated lectures
+	if len(cor.Curriculum) > 0 {
+		const qd = `
+			DELETE FROM 
+				CourseLectures 
+			WHERE 
+				course_id = $1`
+
+		if err := sqldb.ExecContext(ctx, s.log, s.db, qd); err != nil { //????
+			return fmt.Errorf("namedexeccontext: %w", err)
+		}
+
+		for _, lecture := range cor.Curriculum {
+			const q = `
+				INSERT INTO Lectures (
+					title, video_url, public_id, free_preview
+				) VALUES ($1, $2, $3, $4)`
+
+			if err := sqldb.NamedExecContext(ctx, s.log, s.db, q, toDBLecture(lecture)); err != nil {
+				return fmt.Errorf("named exec context: %w", err)
+			}
+
+			const qs = `
+				INSERT INTO lectures (course_id, lecture_id)
+				SELECT $1, id FROM Lectures WHERE public_id = $2`
+
+			curriculumData := toDBCurriculum(lecture.ID, cor.ID)
+
+			if err := sqldb.NamedExecContext(ctx, s.log, s.db, qs, curriculumData); err != nil {
+				return fmt.Errorf("namedexeccontext: %w", err)
+			}
+		}
+	}
+
+	if len(cor.Students) > 0 {
+		// Delete existing students and add updated students
+		const qd = `DELETE FROM CourseStudents WHERE course_id = $1`
+		if err := sqldb.ExecContext(ctx, s.log, s.db, qd); err != nil { //????
+			return fmt.Errorf("namedexeccontext: %w", err)
+		}
+
+		for _, student := range cor.Students {
+			const query = `
+	           INSERT INTO Students (courseId, studentId, studentName, studentEmail, paidAmount)
+	           VALUES (:courseId, :studentId, :studentName, :studentEmail, :paidAmount)`
+
+			if err := sqldb.NamedExecContext(ctx, s.log, s.db, query, toDBStudent(student)); err != nil {
+				return fmt.Errorf("named exec context: %w", err)
+			}
+		}
+	}
+
 	return nil
 }
 
 func (s *Store) QueryAll(ctx context.Context) ([]coursebus.Course, error) {
 	const q = `
-	SELECT 
-		course_id,
-		instructor_id,
-		instructor_name,
-		date,
-		title,
-		category,
-		level,
-		primary_language,
-		subtitle,
-		description,
-		image,
-		welcome_message,
-		pricing,
-		objectives,
-		is_published
-	FROM 
-		courses
-	WHERE 
-		is_published = TRUE;`
+		SELECT 
+			id, instructor_id, instructor_name, date, title, category, level, primary_language, subtitle, description, image, welcome_message, pricing, objectives, is_published 
+		FROM 
+			courses`
 
 	var dbCourses []course
 
@@ -113,12 +210,13 @@ func (s *Store) QueryByID(ctx context.Context, courseID uuid.UUID) (coursebus.Co
 	}
 
 	const q = `
-	SELECT
-	    course_id, instructor_id, instructor_name, date, title, category, level, primary_language, subtitle, description, image, welcome_message, pricing, objectives, is_published
-	FROM
-		courses
-	WHERE
-		course_id_id = :course_id`
+        SELECT 
+            id, instructor_id, instructor_name, date, title, category, level, primary_language, 
+            subtitle, description, image, welcome_message, pricing, objectives, is_published
+        FROM 
+            courses 
+        WHERE 
+            id = $1`
 
 	var dbCor course
 	if err := sqldb.NamedQueryStruct(ctx, s.log, s.db, q, data, &dbCor); err != nil {
@@ -128,5 +226,57 @@ func (s *Store) QueryByID(ctx context.Context, courseID uuid.UUID) (coursebus.Co
 		return coursebus.Course{}, fmt.Errorf("db: %w", err)
 	}
 
-	return toBusCourse(dbCor)
+	//======================================================================================================
+	// Fetch students
+
+	const qs = `
+	    SELECT
+            student_id, student_name, student_email, paid_amount 
+        FROM 
+            students 
+        WHERE 
+            student_id IN (SELECT student_id FROM course_students WHERE course_id = $1)`
+
+	var dbLec []lecture
+	if err := sqldb.NamedQuerySlice(ctx, s.log, s.db, qs, data, &dbLec); err != nil {
+		return coursebus.Course{}, fmt.Errorf("db: (fetchStudents) %w", err)
+	}
+
+	//======================================================================================================
+	// Fetch lectures
+
+	const ql = `
+		SELECT 
+			title, video_url, public_id, free_preview 
+		FROM 
+			lectures 
+		WHERE 
+			id IN (SELECT lecture_id FROM course_curriculum WHERE course_id = $1)`
+
+	var dbStd []student
+	if err := sqldb.NamedQuerySlice(ctx, s.log, s.db, ql, data, &dbStd); err != nil {
+		return coursebus.Course{}, fmt.Errorf("db: (fetchStudents) %w", err)
+	}
+
+	// Map database records to business logic structures
+	course, err := toBusCourse(dbCor)
+	if err != nil {
+		return coursebus.Course{}, err
+	}
+
+	curriculum, err := toBusLectures(dbLec)
+	if err != nil {
+		return coursebus.Course{}, err
+	}
+
+	courseStudents, err := toBusStudents(dbStd)
+	if err != nil {
+		return coursebus.Course{}, err
+	}
+
+	// Attach curriculum and students to the course
+	course.Curriculum = curriculum
+	course.Students = courseStudents
+
+	return course, nil
 }
--- business/domain/coursebus/stores/coursedb/model.go
+++ business/domain/coursebus/stores/coursedb/model.go
@@ -6,8 +6,27 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/kamogelosekhukhune777/lms/business/domain/coursebus"
+	"github.com/kamogelosekhukhune777/lms/business/types/money"
 	"github.com/kamogelosekhukhune777/lms/business/types/name"
 )
+
+type lecture struct {
+	ID          uuid.UUID `json:"lecture_id"`
+	Title       string    `json:"title"`
+	VideoURL    string    `json:"video_url"`
+	PublicID    string    `json:"public_id"`
+	FreePreview bool      `json:"free_preview"`
+}
+
+// Student represents the schema for a student in a course.
+type student struct {
+	ID           uuid.UUID `db:"course_student_id"`
+	CourseID     uuid.UUID `db:"course_id"`
+	StudentID    uuid.UUID `db:"student_id"`
+	StudentName  string    `db:"student_name"`
+	StudentEmail string    `db:"student_email"`
+	PaidAmount   float64   `db:"paid_amount"`
+}
 
 type course struct {
 	ID              uuid.UUID `db:"course_id"`
@@ -27,11 +46,25 @@ type course struct {
 	IsPublished     bool      `db:"is_published"`
 }
 
+type curriculum struct {
+	CourseID  uuid.UUID `db:"course_id"`
+	LectureID uuid.UUID `db:"lecture_id"`
+}
+
+func toDBCurriculum(lectureID, courseID uuid.UUID) curriculum {
+	db := curriculum{
+		CourseID:  courseID,
+		LectureID: lectureID,
+	}
+
+	return db
+}
+
 func toDBCourse(bus coursebus.Course) course {
 	db := course{
 		ID:              bus.ID,
-		InstructorId:    bus.InstructorId,
-		InstructorName:  bus.InstructorName,
+		InstructorId:    bus.InstructorID,
+		InstructorName:  bus.InstructorName.String(),
 		Date:            bus.Date.UTC(),
 		Title:           bus.Title.String(),
 		Category:        bus.Category,
@@ -41,9 +74,33 @@ func toDBCourse(bus coursebus.Course) course {
 		Description:     bus.Description,
 		Image:           bus.Image,
 		WelcomeMessage:  bus.WelcomeMessage,
-		Pricing:         bus.Pricing,
+		Pricing:         bus.Pricing.Value(),
 		Objectives:      bus.Objectives,
 		IsPublished:     bus.IsPublished,
+	}
+
+	return db
+}
+
+func toDBLecture(bus coursebus.Lecture) lecture {
+	db := lecture{
+		ID:          bus.ID,
+		Title:       bus.Title.String(),
+		VideoURL:    bus.VideoURL,
+		FreePreview: bus.FreePreview,
+		PublicID:    bus.PublicID,
+	}
+
+	return db
+}
+
+func toDBStudent(bus coursebus.Student) student {
+	db := student{
+		ID:           bus.ID,
+		CourseID:     bus.CourseID,
+		StudentID:    bus.StudentID,
+		StudentEmail: bus.StudentEmail,
+		PaidAmount:   bus.PaidAmount.Value(),
 	}
 
 	return db
@@ -56,10 +113,20 @@ func toBusCourse(db course) (coursebus.Course, error) {
 		return coursebus.Course{}, fmt.Errorf("parse title: %w", err)
 	}
 
+	instructorName, err := name.Parse(db.InstructorName)
+	if err != nil {
+		return coursebus.Course{}, fmt.Errorf("parse title: %w", err)
+	}
+
+	price, err := money.Parse(db.Pricing)
+	if err != nil {
+		return coursebus.Course{}, fmt.Errorf("parse pricing: %w", err)
+	}
+
 	bus := coursebus.Course{
 		ID:              db.ID,
-		InstructorId:    db.InstructorId,
-		InstructorName:  db.InstructorName,
+		InstructorID:    db.InstructorId,
+		InstructorName:  instructorName,
 		Date:            db.Date,
 		Title:           title,
 		Category:        db.Category,
@@ -69,9 +136,77 @@ func toBusCourse(db course) (coursebus.Course, error) {
 		Description:     db.Description,
 		Image:           db.Image,
 		WelcomeMessage:  db.WelcomeMessage,
-		Pricing:         db.Pricing,
+		Pricing:         price,
 		Objectives:      db.Objectives,
 		IsPublished:     db.IsPublished,
+	}
+
+	return bus, nil
+}
+
+func toBusLecture(db lecture) (coursebus.Lecture, error) {
+	title, err := name.Parse(db.Title)
+	if err != nil {
+		return coursebus.Lecture{}, fmt.Errorf("parse title: %w", err)
+	}
+
+	bus := coursebus.Lecture{
+		ID:          db.ID,
+		Title:       title,
+		VideoURL:    db.VideoURL,
+		FreePreview: db.FreePreview,
+		PublicID:    db.PublicID,
+	}
+
+	return bus, nil
+}
+
+func toBusStudent(db student) (coursebus.Student, error) {
+	studentName, err := name.Parse(db.StudentName)
+	if err != nil {
+		return coursebus.Student{}, fmt.Errorf("parse title: %w", err)
+	}
+
+	paidAmount, err := money.Parse(db.PaidAmount)
+	if err != nil {
+		return coursebus.Student{}, fmt.Errorf("parse pricing: %w", err)
+	}
+
+	bus := coursebus.Student{
+		ID:           db.ID,
+		CourseID:     db.CourseID,
+		StudentID:    db.StudentID,
+		StudentName:  studentName,
+		StudentEmail: db.StudentEmail,
+		PaidAmount:   paidAmount,
+	}
+
+	return bus, nil
+}
+
+func toBusStudents(dbs []student) ([]coursebus.Student, error) {
+	bus := make([]coursebus.Student, len(dbs))
+
+	for i, db := range dbs {
+		var err error
+		bus[i], err = toBusStudent(db)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return bus, nil
+}
+
+func toBusLectures(dbls []lecture) ([]coursebus.Lecture, error) {
+	bus := make([]coursebus.Lecture, len(dbls))
+
+	for i, dbl := range dbls {
+		var err error
+		bus[i], err = toBusLecture(dbl)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return bus, nil
@@ -90,3 +225,6 @@ func toBusCourses(dbs []course) ([]coursebus.Course, error) {
 
 	return bus, nil
 }
+
+//===============================================================================
+//===============================================================================
