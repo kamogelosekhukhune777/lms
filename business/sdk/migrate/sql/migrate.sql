9807ee9cc50bdbb0022139ca64e523aaff3d9770
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2025-01-09 23:56:19 +0200

    tables update

--- business/sdk/migrate/sql/migrate.sql
+++ business/sdk/migrate/sql/migrate.sql
@@ -1,17 +1,13 @@
 -- Version: 1.01
--- Description: Create table users
 CREATE TABLE users (
-    user_id UUID NOT NULL,
+    user_id UUID PRIMARY KEY,
     user_name TEXT NOT NULL,
     user_email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    roles TEXT NOT NULL,
-    enabled BOOLEAN  NOT NULL,
-    PRIMARY KEY (user_id)
+    role TEXT NOT NULL
 );
 
 -- Version: 1.02
--- Description: Create table lectures
 CREATE TABLE lectures (
     lecture_id UUID PRIMARY KEY,
     title TEXT NOT NULL,
@@ -21,12 +17,9 @@ CREATE TABLE lectures (
 );
 
 -- Version: 1.03
--- Description: Create table courses
 CREATE TABLE courses (
     course_id UUID PRIMARY KEY,
     instructor_id UUID NOT NULL,
-    instructor_name TEXT NOT NULL,
-    date TIMESTAMP NOT NULL,
     title TEXT NOT NULL,
     category TEXT NOT NULL,
     level TEXT NOT NULL,
@@ -37,13 +30,14 @@ CREATE TABLE courses (
     welcome_message TEXT,
     pricing NUMERIC(10, 2) NOT NULL,
     objectives TEXT,
-    is_published BOOLEAN NOT NULL DEFAULT FALSE
+    is_published BOOLEAN NOT NULL DEFAULT FALSE,
+    date TIMESTAMP NOT NULL,
+    FOREIGN KEY (instructor_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 -- Version: 1.04
--- Description: Create table curriculum
 CREATE TABLE curriculum (
-    curriculum_id UUID PRIMARY KEY DEFAULT,
+    curriculum_id UUID PRIMARY KEY,
     course_id UUID NOT NULL,
     lecture_id UUID NOT NULL,
     FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE,
@@ -51,56 +45,43 @@ CREATE TABLE curriculum (
 );
 
 -- Version: 1.05
--- Description: Create table course_students
 CREATE TABLE course_students (
     course_student_id UUID PRIMARY KEY,
     course_id UUID NOT NULL,
     student_id UUID NOT NULL,
-    student_name TEXT NOT NULL,
-    student_email TEXT NOT NULL,
     paid_amount NUMERIC(10, 2) NOT NULL,
     FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE,
     FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 -- Version: 1.06
--- Description: Create table student_courses
 CREATE TABLE student_courses (
     student_course_id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
     course_id UUID NOT NULL,
-    title TEXT NOT NULL,
-    instructor_id UUID NOT NULL,
-    instructor_name TEXT NOT NULL,
     date_of_purchase TIMESTAMP NOT NULL,
     course_image TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
 );
 
 -- Version: 1.07
--- Description: Create table orders
 CREATE TABLE orders (
     order_id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
-    user_name TEXT NOT NULL,
-    user_email TEXT NOT NULL,
     order_status TEXT NOT NULL,
     payment_method TEXT NOT NULL,
     payment_status TEXT NOT NULL,
     order_date TIMESTAMP NOT NULL,
     payment_id TEXT,
     payer_id TEXT,
-    instructor_id UUID NOT NULL,
-    instructor_name TEXT NOT NULL,
-    course_image TEXT NOT NULL,
-    course_title TEXT NOT NULL,
     course_id UUID NOT NULL,
     course_pricing NUMERIC(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
 );
 
 -- Version: 1.08
--- Description: Create table lectures_progress
 CREATE TABLE lectures_progress (
     lecture_progress_id UUID PRIMARY KEY,
     lecture_id UUID NOT NULL,
@@ -110,7 +91,6 @@ CREATE TABLE lectures_progress (
 );
 
 -- Version: 1.09
--- Description: Create table course_progress
 CREATE TABLE course_progress (
     course_progress_id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
@@ -120,6 +100,3 @@ CREATE TABLE course_progress (
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
 );
-
-
-
