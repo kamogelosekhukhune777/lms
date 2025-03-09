9ce47b70a050c7afc7d793d245a846533fbccf0b
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2025-03-09 23:30:35 +0200

    web mini framework

--- app/sdk/mux/mux.go
+++ app/sdk/mux/mux.go
@@ -3,18 +3,26 @@
 package mux
 
 import (
+	"context"
 	"net/http"
+
+	"github.com/kamogelosekhukhune777/lms/foundation/logger"
+	"github.com/kamogelosekhukhune777/lms/foundation/web"
 )
 
 // Config contains all the mandatory systems required by handlers.
 type Config struct {
 	Build string
+	Log   *logger.Logger
 }
 
 // WebAPI constructs a http.Handler with all application routes bound.
 func WebAPI(cfg Config) http.Handler {
+	logger := func(ctx context.Context, msg string, args ...any) {
+		cfg.Log.Info(ctx, msg, args...)
+	}
 
-	app := http.NewServeMux()
+	app := web.NewApp(logger)
 
 	return app
 }
---
+++ foundation/web/web.go
@@ -0,0 +1,71 @@
+// Package web contains a small web framework extension.
+package web
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+// Encoder defines behavior that can encode a data model and provide
+// the content type for that encoding.
+type Encoder interface {
+	Encode() (data []byte, contentType string, err error)
+}
+
+// HandlerFunc represents a function that handles a http request within our own
+// little mini framework.
+type HandlerFunc func(ctx context.Context, r *http.Request) Encoder
+
+// Logger represents a function that will be called to add information
+// to the logs.
+type Logger func(ctx context.Context, msg string, args ...any)
+
+// App is the entrypoint into our application and what configures our context
+// object for each of our http handlers. Feel free to add any configuration
+// data/logic on this App struct.
+type App struct {
+	log Logger
+	*http.ServeMux
+}
+
+// NewApp creates an App value that handle a set of routes for the application.
+func NewApp(log Logger) *App {
+	// Create an OpenTelemetry HTTP Handler which wraps our router. This will start
+	// the initial span and annotate it with information about the request/trusted.
+	//
+	// This is configured to use the W3C TraceContext standard to set the remote
+	// parent if a client request includes the appropriate headers.
+	// https://w3c.github.io/trace-context/
+
+	mux := http.NewServeMux()
+
+	return &App{
+		log:      log,
+		ServeMux: mux,
+	}
+}
+
+// HandlerFunc sets a handler function for a given HTTP method and path pair
+// to the application server mux.
+func (a *App) HandlerFunc(method string, group string, path string, handlerFunc HandlerFunc) {
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
+
+		resp := handlerFunc(ctx, r)
+
+		if err := Respond(ctx, w, resp); err != nil {
+			a.log(ctx, "web-respond", "ERROR", err)
+			return
+		}
+	}
+
+	finalPath := path
+	if group != "" {
+		finalPath = "/" + group + path
+	}
+	finalPath = fmt.Sprintf("%s %s", method, finalPath)
+
+	a.ServeMux.HandleFunc(finalPath, h)
+}
