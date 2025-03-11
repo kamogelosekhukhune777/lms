762c64f888487c2a533d042841ed65113a537d56
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2025-03-11 21:23:36 +0200

    userbus specific middleware sup...

--- app/sdk/mid/mid.go
+++ app/sdk/mid/mid.go
@@ -1,7 +1,14 @@
 // Package mid provides app level middleware support.
 package mid
 
-import "github.com/kamogelosekhukhune777/lms/foundation/web"
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/kamogelosekhukhune777/lms/business/domain/userbus"
+	"github.com/kamogelosekhukhune777/lms/foundation/web"
+)
 
 // isError tests if the Encoder has an error inside of it.
 func isError(e web.Encoder) error {
@@ -10,4 +17,40 @@ func isError(e web.Encoder) error {
 		return err
 	}
 	return nil
+}
+
+// ==================================================================================
+type ctxKey int
+
+const (
+	userIDKey ctxKey = iota + 1
+	userKey
+)
+
+func setUserID(ctx context.Context, userID uuid.UUID) context.Context {
+	return context.WithValue(ctx, userIDKey, userID)
+}
+
+// GetUserID returns the user id from the context.
+func GetUserID(ctx context.Context) (uuid.UUID, error) {
+	v, ok := ctx.Value(userIDKey).(uuid.UUID)
+	if !ok {
+		return uuid.UUID{}, errors.New("user id not found in context")
+	}
+
+	return v, nil
+}
+
+func setUser(ctx context.Context, usr userbus.User) context.Context {
+	return context.WithValue(ctx, userKey, usr)
+}
+
+// GetUser returns the user from the context.
+func GetUser(ctx context.Context) (userbus.User, error) {
+	v, ok := ctx.Value(userKey).(userbus.User)
+	if !ok {
+		return userbus.User{}, errors.New("user not found in context")
+	}
+
+	return v, nil
 }
