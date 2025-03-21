8b3fc01aac5fdb36c52049b4d1f98d7c94852305
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2025-03-21 22:19:42 +0200

    cors middleware

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -29,7 +29,8 @@ type Logger func(ctx context.Context, msg string, args ...any)
 type App struct {
 	log Logger
 	*http.ServeMux
-	mw []MidFunc
+	mw      []MidFunc
+	origins []string
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
@@ -44,12 +45,79 @@ func NewApp(log Logger, mw ...MidFunc) *App {
 	}
 }
 
+// EnableCORS enables CORS preflight requests to work in the middleware. It
+// prevents the MethodNotAllowedHandler from being called. This must be enabled
+// for the CORS middleware to work.
+func (a *App) EnableCORS(origins []string) {
+	a.origins = origins
+
+	handler := func(ctx context.Context, r *http.Request) Encoder {
+		return nil
+	}
+	handler = wrapMiddleware([]MidFunc{a.corsHandler}, handler)
+
+	a.HandlerFuncNoMid(http.MethodOptions, "", "/", handler)
+}
+
+func (a *App) corsHandler(webHandler HandlerFunc) HandlerFunc {
+	h := func(ctx context.Context, r *http.Request) Encoder {
+		w := GetWriter(ctx)
+
+		reqOrigin := r.Header.Get("Origin")
+		for _, origin := range a.origins {
+			if origin == "*" || origin == reqOrigin {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "POST, PATCH, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Max-Age", "86400")
+
+		return webHandler(ctx, r)
+	}
+
+	return h
+}
+
 // HandlerFunc sets a handler function for a given HTTP method and path pair
 // to the application server mux.
 func (a *App) HandlerFunc(method string, group string, path string, handlerFunc HandlerFunc, mw ...MidFunc) {
 	handlerFunc = wrapMiddleware(mw, handlerFunc)
 	handlerFunc = wrapMiddleware(a.mw, handlerFunc)
 
+	if a.origins != nil {
+		handlerFunc = wrapMiddleware([]MidFunc{a.corsHandler}, handlerFunc)
+	}
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		v := Values{
+			TraceID: uuid.NewString(),
+		}
+		ctx := setValues(r.Context(), &v)
+		ctx = setWriter(ctx, w)
+
+		resp := handlerFunc(ctx, r)
+
+		if err := Respond(ctx, w, resp); err != nil {
+			a.log(ctx, "web-respond", "ERROR", err)
+			return
+		}
+	}
+
+	finalPath := path
+	if group != "" {
+		finalPath = "/" + group + path
+	}
+	finalPath = fmt.Sprintf("%s %s", method, finalPath)
+
+	a.ServeMux.HandleFunc(finalPath, h)
+}
+
+// HandlerFunc sets a handler function for a given HTTP method and path pair
+// to the application server mux.
+func (a *App) HandlerFuncNoMid(method string, group string, path string, handlerFunc HandlerFunc) {
 	h := func(w http.ResponseWriter, r *http.Request) {
 		v := Values{
 			TraceID: uuid.NewString(),
